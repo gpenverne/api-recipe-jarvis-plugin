c95431eafb978a4e844d30c8d399c3c91fc11365
gpenverne <gpenverne@gmail.com>
2018-01-04 07:07:23 +0100

    wildcard for all

--- install.sh
+++ install.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 # This file will be automatically sourced at the installation of your plugin
 # Use only if you need to perform changes on the user system such as installing software
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-wget "http://$jv_pg_ar_host/jarvis" > /$DIR/fr/commands
--- update.sh
+++ update.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 # This file will be automatically sourced each time your plugin is updated
 # Use only if you need to perform updates on the user systems to support evolution of your plugin
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-wget "http://$jv_pg_ar_host/jarvis" > /$DIR/fr/commands
